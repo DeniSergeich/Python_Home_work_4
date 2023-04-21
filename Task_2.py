74c0e44a81f7ae8a55a33d2312e4e9f34e86862a
DenniSergeich <damned_wolf@mail.ru>
2023-04-21 08:55:30 +0300

    Fix Task 2

--- Task_2.py
+++ Task_2.py
@@ -3,4 +3,6 @@ max = 0
 for i in range(len(list)-1):
     if list[i-1] + list[i] + list[i+1] > max:
         max = list[i-1] + list[i] + list[i+1]
+if list[-2] + list[-1] + list[0] > max:
+        max = list[-2] + list[-1] + list[0]
 print(max)
