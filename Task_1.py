bfd929b483e5000fec3a069169a3cc5ed1d7c216
DenniSergeich <damned_wolf@mail.ru>
2023-04-21 08:32:50 +0300

    Fix Task 1

--- Task_1.py
+++ Task_1.py
@@ -1,4 +1,3 @@
 list_1 = list(map(int, input("Введите список 1: ").split()))
 list_2 = list(map(int, input("Введите список 2: ").split()))
-list = set(list_1 + list_2)
-print(list)
+print(set(list_1 + list_2))
