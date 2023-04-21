fcd17cfa7b6658368b753dd32929cb89dad160e4
DenniSergeich <damned_wolf@mail.ru>
2023-04-21 09:03:19 +0300

    Fix Task 2

--- Task_2.py
+++ Task_2.py
@@ -1,8 +1,6 @@
 list = list(map(int, input("Введите количество ягод на всех кустах: ").split()))
 max = 0
 for i in range(len(list)-1):
-    if list[i-1] + list[i] + list[i+1] > max:
-        max = list[i-1] + list[i] + list[i+1]
-if list[-2] + list[-1] + list[0] > max:
-        max = list[-2] + list[-1] + list[0]
+    if list[i-2] + list[i-1] + list[i] > max:
+        max = list[i-2] + list[i-1] + list[i]
 print(max)
